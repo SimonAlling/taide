98aace7d9dd82d2ccb6c8523c8a5c258838695b7
Simon Alling <alling.simon@gmail.com>
2016-04-20 11:55:02 +0200

    Rewrite sync section in settings
    I have improved both UX (e.g. label strings) and code readability (variable
    names and control flow logic).

--- Taide/app/src/main/java/se/chalmers/taide/SettingsFragment.java
+++ Taide/app/src/main/java/se/chalmers/taide/SettingsFragment.java
@@ -17,9 +17,9 @@ import se.chalmers.taide.model.history.HistoryHandlerFactory;
  */
 public class SettingsFragment extends PreferenceFragment{
 
-    public static final String PREFS_USE_SYNC = "pref_key_use_sync";
-    public static final String PREFS_SYNC_DENY_FORMATS = "pref_key_sync_deny_formats";
-    public static final String PREFS_HISTORY_HANDLER_TYPE = "pref_key_history_handler_type";
+    public static final String PREF_USE_SYNC = "pref_key_use_sync";
+    public static final String PREF_SYNC_FORMATS = "pref_key_sync_formats";
+    public static final String PREF_HISTORY_HANDLER_TYPE = "pref_key_history_handler_type";
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.OnSharedPreferenceChangeListener changeListener = new SharedPreferences.OnSharedPreferenceChangeListener(){
@@ -56,40 +56,38 @@ public class SettingsFragment extends PreferenceFragment{
 
     private void onPreferenceChange(SharedPreferences preferences, String key){
         switch(key){
-            case PREFS_USE_SYNC: case PREFS_SYNC_DENY_FORMATS:  updateSyncPreferences(preferences); break;
-            case PREFS_HISTORY_HANDLER_TYPE:                    updateHistorySettings(preferences);break;
+            case PREF_USE_SYNC: case PREF_SYNC_FORMATS:  updateSyncPreferences(preferences); break;
+            case PREF_HISTORY_HANDLER_TYPE:                    updateHistorySettings(preferences);break;
         }
     }
 
     private void updateSyncPreferences(SharedPreferences preferences){
         if(preferences != null) {
-            int stringRef;
-            //Use sync stuff
-            stringRef = preferences.getBoolean(PREFS_USE_SYNC, true) ? R.string.pref_summary_sync_on : R.string.pref_summary_sync_off;
-            findPreference(PREFS_USE_SYNC).setSummary(getResources().getString(stringRef));
-
             //Sync formats stuff
-            int allowedFormatCount = preferences.getStringSet(PREFS_SYNC_DENY_FORMATS, Collections.EMPTY_SET).size();
-            stringRef = (allowedFormatCount > 0 ? R.string.pref_summary_sync_deny_formats_partly : R.string.pref_summary_sync_deny_formats_none);
-            if (allowedFormatCount == getResources().getStringArray(R.array.file_formats_values).length) {
-                stringRef = R.string.pref_summary_sync_deny_formats_all;
+            // We'll start out with the assumption that no resource types are selected:
+            int stringRef = R.string.pref_summary_sync_formats_none;
+            int allowedResourceTypeCount = preferences.getStringSet(PREF_SYNC_FORMATS, Collections.EMPTY_SET).size();
+            if (allowedResourceTypeCount == getResources().getStringArray(R.array.file_formats_values).length) {
+                // All resource types are selected.
+                stringRef = R.string.pref_summary_sync_formats_all;
+            } else if (allowedResourceTypeCount > 0) {
+                // Some, but not all, resource types are selected.
+                stringRef = R.string.pref_summary_sync_formats_some;
             }
-            if (!preferences.getBoolean(PREFS_USE_SYNC, true)) {
-                stringRef = R.string.pref_summary_sync_off;
-            }
-            findPreference(PREFS_SYNC_DENY_FORMATS).setSummary(getResources().getString(stringRef));
+            System.out.println(findPreference(PREF_SYNC_FORMATS));
+            findPreference(PREF_SYNC_FORMATS).setSummary(getResources().getString(stringRef));
         }
     }
 
     private void updateHistorySettings(SharedPreferences preferences){
         if(preferences != null){
-            String type = preferences.getString(PREFS_HISTORY_HANDLER_TYPE, "time");
+            String type = preferences.getString(PREF_HISTORY_HANDLER_TYPE, "time");
             switch(type){
                 case "word":    HistoryHandlerFactory.setHistoryHandlerType(HistoryHandlerFactory.HistoryHandlerType.WORD);break;
                 case "time":
                 default:        HistoryHandlerFactory.setHistoryHandlerType(HistoryHandlerFactory.HistoryHandlerType.TIME);break;
             }
-            Preference p = findPreference(PREFS_HISTORY_HANDLER_TYPE);
+            Preference p = findPreference(PREF_HISTORY_HANDLER_TYPE);
             if(p != null){
                 p.setSummary("Current mode is " + type);
             }
