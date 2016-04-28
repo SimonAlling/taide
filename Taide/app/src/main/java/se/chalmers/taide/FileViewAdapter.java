42282634299a48e9bf4dc32389010bf37961a914
Simon Alling <alling.simon@gmail.com>
2016-04-28 11:50:00 +0200

    Improve code style in FileViewAdapter

--- Taide/app/src/main/java/se/chalmers/taide/FileViewAdapter.java
+++ Taide/app/src/main/java/se/chalmers/taide/FileViewAdapter.java
@@ -29,22 +29,22 @@ public class FileViewAdapter extends ArrayAdapter<CodeFile>{
     public View getView(int position, View convertView, ViewGroup parent) {
         Context context;
         View view;
-        if(convertView == null){
-            context = (parent!=null?parent.getContext():this.context);
+        if (convertView == null) {
+            context = (parent != null ? parent.getContext() : this.context);
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.icon_text_item_view_files, parent, false);
-        }else{
+        } else {
             context = convertView.getContext();
             view = convertView;
         }
 
         TextView textView = (TextView) view.findViewById(R.id.item_name);
         ImageView icon = (ImageView) view.findViewById(R.id.item_icon);
-        if(position == 0){
+        if (position == 0) {
             textView.setText(R.string.upOneLevel);
             textView.setEnabled(canStepUpOneLevel);
             icon.setImageResource(getReferenceFromStyle(context, R.attr.iconUpOneFileLevel, R.drawable.ic_up_one_file_level_white));
-        }else{
+        } else {
             CodeFile f = getItem(position);
             textView.setText(f.getName());
             if (f.isDirectory()) {
@@ -61,26 +61,21 @@ public class FileViewAdapter extends ArrayAdapter<CodeFile>{
         return view;
     }
 
-    private int getReferenceFromStyle(Context context, int attribute, int def){
-        TypedValue tv = new TypedValue();
-        context.getTheme().resolveAttribute(attribute, tv, false);
-        return tv.data==0?def:tv.data;
+    private int getReferenceFromStyle(Context context, int attribute, int def) {
+        TypedValue typedValue = new TypedValue();
+        context.getTheme().resolveAttribute(attribute, typedValue, false);
+        return typedValue.data == 0 ? def : typedValue.data;
     }
 
     @Override
     public int getItemViewType(int position) {
-        if(position == 0){
-            return 1;
-        }else{
-            return 0;
-        }
+        return position == 0 ? 1 : 0;
     }
 
-    private static CodeFile[] prepareArray(CodeFile[] array){
+    private static CodeFile[] prepareArray(CodeFile[] array) {
         CodeFile[] arr = new CodeFile[array.length+1];
         arr[0] = null;
         System.arraycopy(array, 0, arr, 1, array.length);
         return arr;
     }
 }
-
