9b4e2ce5d517b5b3b183f6a3474fce210632986e
Matz Larsson <matzlarsson94@gmail.com>
2016-05-11 11:27:09 +0200

    Change action menu detection to use angles
    The user often missed the action button when the functionality only
    checked if the finger was above the button. Now it instead checks if the
    fingerpress is in the correct angle. Note that this functionality might
    need some tuning to be correct, now it seems to have a small
    missdirection (if you try hard you notice). This works better than the
    previous version though.

--- Taide/app/src/main/java/se/chalmers/taide/RadialActionMenuLayout.java
+++ Taide/app/src/main/java/se/chalmers/taide/RadialActionMenuLayout.java
@@ -12,8 +12,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import se.chalmers.taide.util.MathUtil;
+
 /**
  * Created by Matz on 2016-04-08.
+ *
+ * Custom class for handling a radial action menu, consisting of a main button that expands
+ * several other button. These buttons can be linked to action, making this ui element
+ * appropriate for fast access tools etc.
+ *
+ * NOTE: Be sure to call setButtons(String[] labels) to make the action menu visible.
  */
 public class RadialActionMenuLayout extends RelativeLayout{
 
@@ -46,6 +54,11 @@ public class RadialActionMenuLayout extends RelativeLayout{
     // Otherwise, X_LOWERMOST_BUTTON_ANGLE and X_UPPERMOST_BUTTON_ANGLE will decide the button placement.
     private static final boolean USE_SYMMETRY = true;
 
+    //Determines the distance factor from the main button the button should be triggered. This
+    //factor is multiplied with the width of the main button.
+    private static final int MIN_DISTANCE_DETECTION_FACTOR = 1;
+    private static final int MAX_DISTANCE_DETECTION_FACTOR = 5;
+
 
     private View mainView;
     private Button mainButton;
@@ -56,6 +69,8 @@ public class RadialActionMenuLayout extends RelativeLayout{
     private int animationDuration;
     private int selectionAnimationDuration;
     private int buttonColor;
+
+    private int selectedButton = -1;
 
 
     public RadialActionMenuLayout(final Context context){
@@ -96,13 +111,200 @@ public class RadialActionMenuLayout extends RelativeLayout{
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     show();
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (!activateButton(event.getRawX(), event.getRawY())) {
-                        hide();
+                    if(selectedButton >= 0){
+                        activateButton(selectedButton);
+                        buttons[selectedButton].findViewById(R.id.radialButton).getBackground().setAlpha(255);
+                    }
+                    selectedButton = -1;
+                    hide();
+                } else if (event.getAction() == MotionEvent.ACTION_MOVE){
+                    int buttonSelection = getSelectedButton(event.getRawX(), event.getRawY());
+                    if(buttonSelection != RadialActionMenuLayout.this.selectedButton) {
+                        for (int i = 0; i<buttons.length; i++) {
+                            buttons[i].findViewById(R.id.radialButton).getBackground().setAlpha(i==buttonSelection ? 128 : 255);
+                        }
+                        RadialActionMenuLayout.this.selectedButton = buttonSelection;
                     }
                 }
                 return false;
             }
         });
+    }
+
+    private void generateButtons(int buttonCount) {
+        View[] newButtons = new View[buttonCount];
+        OnActionButtonTriggeredListener[] newButtonListeners = new OnActionButtonTriggeredListener[buttonCount];
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        RelativeLayout parent = (RelativeLayout) mainView;
+        for (int i = 0; i < buttonCount; i++) {
+            if (this.buttons != null && i < this.buttons.length){
+                newButtons[i] = this.buttons[i];
+                newButtonListeners[i] = this.buttonListeners[i];
+
+                // Update color:
+                Button b = (Button)newButtons[i].findViewById(R.id.radialButton);
+                if(b.getBackground() instanceof GradientDrawable) {
+                    ((GradientDrawable) b.getBackground()).setColor(getResources().getColor(buttonColor));
+                }
+            } else {
+                View view = getCircleButton(inflater, parent, "");
+                view.setVisibility(View.GONE);
+                newButtons[i] = view;
+            }
+        }
+
+        this.buttons = newButtons;
+        this.buttonListeners = newButtonListeners;
+    }
+
+    private View getCircleButton(LayoutInflater inflater, ViewGroup parent, String text) {
+        View view = inflater.inflate(R.layout.radial_button, null);
+        parent.addView(view);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        params.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+        params.addRule(alignment == Alignment.RIGHT ? RelativeLayout.ALIGN_PARENT_RIGHT : RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        view.setLayoutParams(params);
+
+        Button button = ((Button) view.findViewById(R.id.radialButton));
+        button.setText(text);
+        if (button.getBackground() instanceof GradientDrawable) {
+            ((GradientDrawable) button.getBackground()).setColor(getResources().getColor(buttonColor));
+        }
+
+        return view;
+    }
+
+    private int getSelectedButton(double x, double y) {
+        //Retrieve the location of the main button
+        int[] location = new int[2];
+        mainButton.getLocationOnScreen(location);
+        location[0] += mainButton.getWidth()/2;
+        location[1] += mainButton.getHeight()/2;
+
+        //Calculate the angle towards the event
+        int steps = buttons.length-1;
+        double angle = MathUtil.getAngle(location[0], location[1], x, y);
+        double startAngle = MathUtil.convertAngleIntoNormalRange(getStartAngle(alignment, steps));
+        double angleChange = getAngleChange(alignment, steps);
+
+        //If the range (startAngle to (startAngle+angleChange*steps)) passes the rightmost of the
+        //x-axis, the angle might be translated -360 degrees. Therefore we have to translate it back.
+        if(angle<startAngle){
+            angle += 360;
+        }
+
+        //Check that it is in the correct interval
+        if(angle >= startAngle && angle <= startAngle+angleChange*(steps+1)){
+            int index = (int)((angle-startAngle)/angleChange);
+            //Check that it is far/close enough.
+            if(index<buttons.length && MathUtil.distanceIsGreaterThan(mainButton.getWidth() * MIN_DISTANCE_DETECTION_FACTOR, location[0], location[1], x, y) &&
+                                       MathUtil.distanceIsSmallerThan(mainButton.getWidth() * MAX_DISTANCE_DETECTION_FACTOR, location[0], location[1], x, y)){
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    private void activateButton(int index){
+        //Calculate values
+        final View activeButton = buttons[index];
+        final float scaleDiff = 1.2f;
+        final float translateX = ((float) activeButton.getWidth())*(scaleDiff-1.0f)/2.0f;
+        final float translateY = ((float) activeButton.getHeight())*(scaleDiff-1.0f)/2.0f;
+
+        //Perform the animation
+        activeButton.animate().setDuration(selectionAnimationDuration).scaleX(scaleDiff).scaleY(scaleDiff).
+                translationX(activeButton.getTranslationX() - translateX).translationY(activeButton.getTranslationY()-translateY*2).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                activeButton.animate().setDuration(0).scaleX(1f).scaleY(1f).translationX(0).translationY(0).start();
+                hide();
+            }
+        }).start();
+
+        //Notify listeners
+        if (buttonListeners[index] != null) {
+            buttonListeners[index].actionButtonTriggered(index);
+        }
+    }
+
+    private void show() {
+        final double childButtonDistance = mainButton.getWidth()/2 * CHILD_BUTTON_DISTANCE_FACTOR; // division by 2 to get mother button radius
+        final int steps = buttons.length - 1;
+        final double angleChange = getAngleChange(alignment, steps);
+        final double startAngle = getStartAngle(alignment, steps);
+        for (int i = 0; i < buttons.length; i++) {
+            double angle = startAngle + i*angleChange;
+            double radians = Math.toRadians(angle);
+            float x = (float) (childButtonDistance * Math.cos(radians));
+            float y = (float) (childButtonDistance * Math.sin(radians));
+            buttons[i].setVisibility(View.VISIBLE);
+            buttons[i].animate().setDuration(animationDuration).translationX(x).translationY(y).start();
+        }
+    }
+
+    private void hide() {
+        for (View button : buttons) {
+            button.animate().setDuration(animationDuration).translationX(0f).translationY(0f).start();
+            button.setVisibility(View.GONE);
+        }
+    }
+
+    private static double getAngleChange(Alignment alignment, int steps){
+        // The angle between two adjacent buttons:
+        // The special case for buttons.length == 1 is there because otherwise angleChange will be
+        // NaN if symmetry is not used, and then we cannot support exactly one button.
+        if(USE_SYMMETRY){
+            return ANGLE_BETWEEN_ADJACENT_BUTTONS;
+        }else{
+            double totalAngle = alignment == Alignment.LEFT
+                    ?  LEFT_UPPERMOST_BUTTON_ANGLE -  LEFT_LOWERMOST_BUTTON_ANGLE
+                    : RIGHT_UPPERMOST_BUTTON_ANGLE - RIGHT_LOWERMOST_BUTTON_ANGLE;
+            return steps > 0 ? totalAngle / steps : 0;
+        }
+    }
+
+    private static double getStartAngle(Alignment alignment, int steps){
+        // The angle of the first button in clockwise order:
+        // Note that 0 deg is an angle pointing to the right and angles increase CLOCKWISE!
+        if(USE_SYMMETRY){
+            final double totalAngle = steps * ANGLE_BETWEEN_ADJACENT_BUTTONS;
+            return alignment == Alignment.LEFT
+                    ?   0 - SYMMETRY_ANGLE - (totalAngle/2)
+                    : 180 + SYMMETRY_ANGLE - (totalAngle/2);
+        }else{
+            return alignment == Alignment.LEFT
+                    ?   0 -  LEFT_UPPERMOST_BUTTON_ANGLE
+                    : 180 + RIGHT_LOWERMOST_BUTTON_ANGLE;
+        }
+    }
+
+
+    /**
+     * Takes an array of strings and creates buttons with those labels.
+     * There will be NO BUTTONS until this is called.
+     * @param labels The desired labels of the buttons in clockwise order
+     */
+    public void setButtons(String[] labels) {
+        generateButtons(labels.length);
+        for (int i = 0; i < labels.length; i++) {
+            ((Button) buttons[i].findViewById(R.id.radialButton)).setText(labels[i]);
+        }
+        invalidate();
+        requestLayout();
+    }
+
+    public void setActionForAll(OnActionButtonTriggeredListener listener) {
+        for (int i = 0; i < buttonListeners.length; i++) {
+            buttonListeners[i] = listener;
+        }
+    }
+
+    public void setAction(int buttonIndex, OnActionButtonTriggeredListener listener) {
+        if (buttonIndex >= 0 && buttonIndex < buttonListeners.length) {
+            buttonListeners[buttonIndex] = listener;
+        }
     }
 
     public void setAlignment(Alignment alignment) {
@@ -163,141 +365,6 @@ public class RadialActionMenuLayout extends RelativeLayout{
             ((Button) buttons[buttonIndex].findViewById(R.id.radialButton)).setText(label);
             invalidate();
             requestLayout();
-        }
-    }
-
-    /**
-     * Takes an array of strings and creates buttons with those labels.
-     * There will be NO BUTTONS until this is called.
-     * @param labels The desired labels of the buttons in clockwise order
-     */
-    public void setButtons(String[] labels) {
-        generateButtons(labels.length);
-        for (int i = 0; i < labels.length; i++) {
-            ((Button) buttons[i].findViewById(R.id.radialButton)).setText(labels[i]);
-        }
-        invalidate();
-        requestLayout();
-    }
-
-    public void setAction(int buttonIndex, OnActionButtonTriggeredListener listener) {
-        if (buttonIndex >= 0 && buttonIndex < buttonListeners.length) {
-            buttonListeners[buttonIndex] = listener;
-        }
-    }
-
-    public void setActionForAll(OnActionButtonTriggeredListener listener) {
-        for (int i = 0; i < buttonListeners.length; i++) {
-            buttonListeners[i] = listener;
-        }
-    }
-
-    private View getCircleButton(LayoutInflater inflater, ViewGroup parent, String text) {
-        View view = inflater.inflate(R.layout.radial_button, null);
-        parent.addView(view);
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
-        params.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
-        params.addRule(alignment == Alignment.RIGHT ? RelativeLayout.ALIGN_PARENT_RIGHT : RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        view.setLayoutParams(params);
-
-        Button button = ((Button) view.findViewById(R.id.radialButton));
-        button.setText(text);
-        if (button.getBackground() instanceof GradientDrawable) {
-            ((GradientDrawable) button.getBackground()).setColor(getResources().getColor(buttonColor));
-        }
-
-        return view;
-    }
-
-    private void generateButtons(int buttonCount) {
-        View[] newButtons = new View[buttonCount];
-        OnActionButtonTriggeredListener[] newButtonListeners = new OnActionButtonTriggeredListener[buttonCount];
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        RelativeLayout parent = (RelativeLayout) mainView;
-        for (int i = 0; i < buttonCount; i++) {
-            if (this.buttons != null && i < this.buttons.length){
-                newButtons[i] = this.buttons[i];
-                newButtonListeners[i] = this.buttonListeners[i];
-
-                // Update color:
-                ((GradientDrawable) newButtons[i].findViewById(R.id.radialButton).getBackground()).setColor(getResources().getColor(buttonColor));
-            } else {
-                View view = getCircleButton(inflater, parent, "");
-                view.setVisibility(View.GONE);
-                newButtons[i] = view;
-            }
-        }
-
-        this.buttons = newButtons;
-        this.buttonListeners = newButtonListeners;
-    }
-
-    private void show() {
-        final double childButtonDistance = mainButton.getWidth()/2 * CHILD_BUTTON_DISTANCE_FACTOR; // division by 2 to get mother button radius
-        final int steps = buttons.length - 1;
-        // The angle between the lowermost and the uppermost button:
-        final double totalAngle_symmetry = steps * ANGLE_BETWEEN_ADJACENT_BUTTONS;
-        final double totalAngle_distribution = alignment == Alignment.LEFT
-                                             ?  LEFT_UPPERMOST_BUTTON_ANGLE -  LEFT_LOWERMOST_BUTTON_ANGLE
-                                             : RIGHT_UPPERMOST_BUTTON_ANGLE - RIGHT_LOWERMOST_BUTTON_ANGLE;
-        final double totalAngle = USE_SYMMETRY ? totalAngle_symmetry : totalAngle_distribution;
-        // The angle between two adjacent buttons:
-        // The special case for buttons.length == 1 is there because otherwise angleChange will be
-        // NaN if symmetry is not used, and then we cannot support exactly one button.
-        final double angleChange_symmetry = ANGLE_BETWEEN_ADJACENT_BUTTONS;
-        final double angleChange_distribution = steps > 0 ? totalAngle / steps : 0;
-        final double angleChange = USE_SYMMETRY ? angleChange_symmetry : angleChange_distribution;
-        // The angle of the first button in clockwise order:
-        // Note that 0 deg is an angle pointing to the right and angles increase CLOCKWISE!
-        final double startAngle_symmetry = alignment == Alignment.LEFT
-                                         ?   0 - SYMMETRY_ANGLE - (totalAngle/2)
-                                         : 180 + SYMMETRY_ANGLE - (totalAngle/2);
-        final double startAngle_distribution = alignment == Alignment.LEFT
-                                             ?   0 -  LEFT_UPPERMOST_BUTTON_ANGLE
-                                             : 180 + RIGHT_LOWERMOST_BUTTON_ANGLE;
-        final double startAngle = USE_SYMMETRY ? startAngle_symmetry : startAngle_distribution;
-        for (int i = 0; i < buttons.length; i++) {
-            double angle = startAngle + i*angleChange;
-            double radians = Math.toRadians(angle);
-            float x = (float) (childButtonDistance * Math.cos(radians));
-            float y = (float) (childButtonDistance * Math.sin(radians));
-            buttons[i].setVisibility(View.VISIBLE);
-            buttons[i].animate().setDuration(animationDuration).translationX(x).translationY(y).start();
-        }
-    }
-
-    private boolean activateButton(double x, double y) {
-        for (int i = 0; i < buttons.length; i++){
-            int[] location = new int[2];
-            View view = buttons[i].findViewById(R.id.radialButton);
-            view.getLocationOnScreen(location);
-            if (x > location[0] && x < location[0]+view.getWidth() && y > location[1] && y < location[1]+view.getHeight()) {
-                final View activeButton = buttons[i];
-                final float scaleDiff = 1.2f;
-                final float translateX = ((float) view.getWidth())*(scaleDiff-1.0f)/2.0f;
-                final float translateY = ((float) view.getHeight())*(scaleDiff-1.0f)/2.0f;
-                activeButton.animate().setDuration(selectionAnimationDuration).scaleX(scaleDiff).scaleY(scaleDiff).
-                        translationX(activeButton.getTranslationX() - translateX).translationY(activeButton.getTranslationY()-translateY*2).withEndAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        activeButton.animate().setDuration(0).scaleX(1f).scaleY(1f).translationX(0).translationY(0).start();
-                        hide();
-                    }
-                }).start();
-                if (buttonListeners[i] != null) {
-                    buttonListeners[i].actionButtonTriggered(i);
-                }
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private void hide() {
-        for (View button : buttons) {
-            button.animate().setDuration(animationDuration).translationX(0f).translationY(0f).start();
-            button.setVisibility(View.GONE);
         }
     }
 
--- Taide/app/src/main/java/se/chalmers/taide/util/MathUtil.java
+++ Taide/app/src/main/java/se/chalmers/taide/util/MathUtil.java
@@ -15,5 +15,37 @@ public abstract class MathUtil {
         final double factor = Math.pow(10, decimals); // e.g. 1000 if decimals == 3
         return Math.round(x * factor) / factor;
     }
+
+
+
+    public static boolean distanceIsGreaterThan(double maxLimit, double x1, double y1, double x2, double y2){
+        return Math.pow(Math.abs(x1-x2), 2)+Math.pow(Math.abs(y1-y2), 2) > Math.pow(maxLimit, 2);
+    }
+
+    public static boolean distanceIsSmallerThan(double maxLimit, double x1, double y1, double x2, double y2){
+        return Math.pow(Math.abs(x1-x2), 2)+Math.pow(Math.abs(y1-y2), 2) < Math.pow(maxLimit, 2);
+    }
+
+    public static double convertAngleIntoNormalRange(double angle){
+        while(angle<0){
+            angle += 360;
+        }
+
+        return angle%360;
+    }
+
+    public static double getAngle(double x1, double y1, double x2, double y2){
+        final double dx = x2-x1, dy = y2-y1;
+        if(dx != 0) {
+            double angle = Math.toDegrees(Math.atan(dy/dx));
+            if(dx<0){
+                return 180 + angle;
+            }else{
+                return dy>0 ? angle : 360 + angle;
+            }
+        }else{
+            return y2>y1 ? 90 : 270;
+        }
+    }
 }
 
